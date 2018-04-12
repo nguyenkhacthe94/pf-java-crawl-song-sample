c66100b4b8befeabbcab41bb277135f0b7b080a0
khacthe <nguyenkhacthe94@gmail.com>
2018-04-12 10:36:50 +0700

    Crawl Song sample

---
+++ CrawlSongSample.java
@@ -0,0 +1,36 @@
+import java.net.*;
+import java.util.regex.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class CrawlSongSample {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+
+            // open the stream and put it into BufferedReader
+            Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
+            scanner.useDelimiter("\\Z");
+            String content = scanner.next();
+            // close scanner
+            scanner.close();
+
+            // remove all new line
+            content = content.replaceAll("\\n+", "");
+
+            // regex
+            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group(1));
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
